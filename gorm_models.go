c88bfc7e6f17b1cb6facb569c7206815be7712d8
Mgen <m@mgenware.com>
2019-02-15 22:36:58 +0800

    Populate post table

--- gorm_models.go
+++ gorm_models.go
@@ -1,9 +1,15 @@
 package main
 
-import "github.com/jinzhu/gorm"
-
 type User struct {
-	gorm.Model
-	Name string
-	Age  int
+	ID   uint   `gorm:"primary_key"`
+	Name string `gorm:"not null"`
+	Age  int    `gorm:"not null"`
+}
+
+type Post struct {
+	ID      uint   `gorm:"primary_key"`
+	Title   string `gorm:"not null"`
+	Content string `sql:"type:text;gorm:"not null""`
+	User    User   `gorm:"foreignkey:UserID"`
+	UserID  uint   `gorm:"not null"`
 }
--- main_test.go
+++ main_test.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"testing"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
 
 func TestMain(m *testing.M) {
 	db, err := gorm.Open("mysql", "root:123456@/mingru_benchmarks?charset=utf8&parseTime=True&loc=Local")
@@ -17,7 +28,7 @@ func TestMain(m *testing.M) {
 	defer db.Close()
 
 	for _, model := range []interface{}{
-		User{},
+		User{}, Post{},
 	} {
 		if err := db.AutoMigrate(model).Error; err != nil {
 			panic(err)
@@ -26,9 +37,14 @@ func TestMain(m *testing.M) {
 
 	// Adding users
 	for i := 0; i < 100; i++ {
-		user := User{Name: fmt.Sprintf("u-%v", i), Age: i}
-		db.NewRecord(user)
+		user := User{Name: fmt.Sprintf(RandStringRunes(10)), Age: i}
 		db.Create(&user)
+
+		// Adding 2 posts per user
+		for j := 0; j < 2; j++ {
+			post := Post{Title: RandStringRunes(20), Content: RandStringRunes(200), User: user}
+			db.Create(&post)
+		}
 	}
 
 	os.Exit(m.Run())
