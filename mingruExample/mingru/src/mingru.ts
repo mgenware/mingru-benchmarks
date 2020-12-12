2ac5545ca3620342428156cf8bd2126edf3822ee
Mgen <m@mgenware.com>
2020-12-13 00:30:05 +0800

    Upgrade to the latest mingru-models and mingru

--- mingruExample/mingru/src/mingru.ts
+++ mingruExample/mingru/src/mingru.ts
@@ -5,7 +5,7 @@ import actions from './actions/actions';
 (async () => {
   const dialect = new mr.MySQL();
   const builder = new mr.Builder(dialect, '../da/', {
-    cleanBuild: true, // Cleans build directory on each build
+    cleanBuild: true, // Cleans build directory on each build.
   });
 
   await builder.buildAsync(async () => {
